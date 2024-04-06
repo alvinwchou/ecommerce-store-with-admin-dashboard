c23f5e1708913664f41a993f9fcb67bb83e19bf8
Alvin C <alvinwchou@gmail.com>
2024-04-06 19:03:05 -0400

    feat: :sparkles: Finally able to pull data from firestore and making it usable

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,7 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { PageHeader } from "../_components/PageHeader";
 import Link from "next/link";
-import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { collection, getDocs } from "firebase/firestore";
+import db from "@/app/firebase";
+import { CheckCircle2, XCircle } from "lucide-react";
+import { formatCurrency, formatNumber } from "@/lib/formatters";
+
+interface Product {
+  id: string;
+  name: string;
+  pricePaidInCents: number;
+  isAvailableForPurchase: boolean;
+  orderCount: number;
+}
 
 export default function AdminProductPage() {
   return (
@@ -17,7 +36,34 @@ export default function AdminProductPage() {
   );
 }
 
-function ProductsTable() {
+async function ProductsTable() {
+  // create a reference to the Product database
+  const productRef = collection(db, "Product");
+  const productsSnapshot = await getDocs(productRef);
+
+  const products: Product[] = [];
+
+  productsSnapshot.forEach((doc) => {
+    const productData = doc.data();
+
+    if (productData) {
+      // create a new object
+      const product: Product = {
+        id: doc.id,
+        name: productData.name,
+        pricePaidInCents: productData.pricePaidInCents,
+        isAvailableForPurchase: productData.isAvailableForPurchase,
+        orderCount: productData.order?.length() || 0,
+      };
+
+      // push it to products
+      products.push(product);
+    }
+  });
+
+  // return message if no products found
+  if (products.length === 0) return <p>No products found</p>;
+
   return (
     <Table>
       <TableHeader>
@@ -34,7 +80,23 @@ function ProductsTable() {
         </TableRow>
       </TableHeader>
       <TableBody>
-
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell>
+              {product.isAvailableForPurchase ? (
+                <>
+                  <CheckCircle2 />
+                  <span className="sr-only">Available</span>
+                </>
+              ) : (
+                <>
+                  <XCircle />
+                  <span className="sr-only">Unavailable</span>
+                </>
+              )}
+            </TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   );
